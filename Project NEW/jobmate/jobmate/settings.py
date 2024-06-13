07327390966c58f5faa1cee8825f8bb5e470aba9
Sobin Sebastian <117562463+SobinSebastian@users.noreply.github.com>
2024-06-13 22:20:03 +0530

    se_up

--- Project NEW/jobmate/jobmate/settings.py
+++ Project NEW/jobmate/jobmate/settings.py
@@ -204,6 +204,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jobmate2023@gmail.com'
-EMAIL_HOST_PASSWORD ='paxv hoct zgjb ztrc'
+EMAIL_HOST_USER = 'jobmate2023@gmail.com' #Replace with your Gmail Address
+EMAIL_HOST_PASSWORD ='qwer tyui opas dfgh' # Replace with Your app password
 
