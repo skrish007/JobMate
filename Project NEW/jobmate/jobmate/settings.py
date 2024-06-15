f4cfa4a5d1edf3e69f867e4c0bbdf1c305c92d14
K Shyam Krishnan <119836593+skrish007@users.noreply.github.com>
2024-06-15 16:13:32 +0530

    Update settings.py

--- Project NEW/jobmate/jobmate/settings.py
+++ Project NEW/jobmate/jobmate/settings.py
@@ -205,5 +205,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jobmate2023@gmail.com'
-EMAIL_HOST_PASSWORD ='paxv hoct zgjb ztrc'
+EMAIL_HOST_PASSWORD =''
 
